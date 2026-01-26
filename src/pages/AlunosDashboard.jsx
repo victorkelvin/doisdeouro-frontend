79241bf42e575ece0e5b4787cb8c2e9b4d971be7
victorkelvin <victorkelvin@gmail.com>
2026-01-26 14:32:04 -0300

    fix: Correct SVG path directions for form visibility toggle in AlunosDashboard

--- src/pages/AlunosDashboard.jsx
+++ src/pages/AlunosDashboard.jsx
@@ -222,14 +222,14 @@ const AlunosDashboard = () => {
                     {isFormVisible ? (
                         <>
                             <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" />
                             </svg>
                             <span>Ocultar Formulário</span>
                         </>
                     ) : (
                         <>
                             <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" />
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
                             </svg>
                             <span>Mostrar Formulário</span>
                         </>
