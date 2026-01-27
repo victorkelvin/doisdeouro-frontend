a23d76707b1f391c049e0758716d4c8ae3f744dd
victorkelvin <victorkelvin@gmail.com>
2026-01-26 21:12:50 -0300

    fix: Remove unused useNavigate import from AlunoRegister component

--- src/pages/AlunoRegister.jsx
+++ src/pages/AlunoRegister.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import useAlunoForm from '../hooks/useAlunoForm';
 import { noLoginRequest } from '../services/baseApi';
 import { registerAlunoWithToken } from '../services/alunosApi';
